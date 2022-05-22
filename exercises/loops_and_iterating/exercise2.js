7a26b97d1b2f8de2a943c71d42f65a0d3043041e
Maggie Clerkin <maggietclerkin@gmail.com>
2022-05-22 08:05:32 -0400

    Updated Loops and Iterating Exercises

---
+++ exercises/loops_and_iterating/exercise2.js
@@ -0,0 +1,27 @@
+/*
+Exercise 2:
+
+Write a function that computes and returns the factorial of a number by using a for loop. 
+The factorial of a positive integer n, signified by n!, is defined as the product 
+of all integers between 1 and n, inclusive:
+
+n!	Expansion	          Result
+1!	1	                  1
+2!	1 * 2	              2
+3!	1 * 2 * 3	          6
+4!	1 * 2 * 3 * 4	      24
+5!	1 * 2 * 3 * 4 * 5	  120
+
+You may assume that the argument is always a positive integer.
+
+Examples:
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
+
+*/
